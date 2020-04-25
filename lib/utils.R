d85d81b6b872611eaefaf1b2ab7ecf5473777b8f
fuguigui <fgr079@126.com>
2020-04-25 15:33:05 +0800

    newest report

--- lib/utils.R
+++ lib/utils.R
@@ -75,6 +75,61 @@ GetNeighData<-function(name, spath,nneigh,fs,path,start,end){
 }
 
 
+multiplot <- function(..., plotlist=NULL, file, cols=1,
+                      layout=NULL, horizontal=FALSE, e=0.15) {
+  require(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots = c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  #message(paste0('>>>>>>>INFO: num plots 2 = ', numPlots), '\n')
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout = matrix(seq(1, cols * ceiling(numPlots/cols)),
+                    ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    
+    ## set up heights/widths of plots
+    
+    # extra height needed for last plot (vertical layout),
+    # or extra width for first plot (horizontal layout)
+    hei = rep(1, numPlots)
+    # bottom plot is taller
+    hei[numPlots] = hei[numPlots]*(1+e)
+    wid = rep(1, numPlots)
+    # first left plot is wider
+    wid[1] = wid[1]*(1+e)
+    # Set up the page
+    grid.newpage()
+    if(horizontal){
+      pushViewport(viewport(layout = grid.layout(nrow(layout),
+                                                 ncol(layout), widths=wid)))
+    }else{
+      pushViewport(viewport(layout = grid.layout(nrow(layout),
+                                                 ncol(layout), heights=hei)))
+      
+    }
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get i,j matrix positions of the regions containing this subplot
+      matchidx = as.data.frame(which(layout == i, arr.ind = TRUE))
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
 
 ReadCovid<-function(fname){
   #covid1<-as.data.frame(t(read.csv(paste0(path,fname),header = F, stringsAsFactors = F)),stringsAsFactors = F)
